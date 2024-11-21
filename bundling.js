2e92326f3ef448480ddc14c0b7ed04e07290488e
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-11-21 11:33:27 +0100

    Make the validator available via the bundle

--- bundling.js
+++ bundling.js
@@ -1,5 +1,7 @@
 import { Parser, Store, Writer } from "n3"
 import { QueryEngine } from "@comunica/query-sparql-rdfjs"
+import Validator from "shacl-engine/Validator.js"
+import rdf from "rdf-ext"
 
 export function newParser() {
     return new Parser()
@@ -17,4 +19,12 @@ export function newQueryEngine() {
     return new QueryEngine()
 }
 
-window.bundle = { newParser, newWriter, newStore, newQueryEngine }
+export function getRdfFactory() {
+    return rdf
+}
+
+export function newValidator(dataset, options) {
+    return new Validator(dataset, options)
+}
+
+window.bundle = { newParser, newWriter, newStore, newQueryEngine, getRdfFactory, newValidator }
