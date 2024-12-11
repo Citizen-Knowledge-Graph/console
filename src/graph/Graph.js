5aa810ceab3a6d8a75b55263b4f2066a5c043c14
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-12-11 22:10:39 +0100

    Run is a while loop over step()

--- src/graph/Graph.js
+++ src/graph/Graph.js
@@ -3,27 +3,32 @@ export class Graph {
     constructor() {
         this.nodesMap = {}
         this.edgesMap = {}
+        this.stepCounter = 0
     }
 
     getNodeByEditorId(editorId) {
         return Object.values(this.nodesMap).find(node => node.editorId.toString() === editorId)
     }
 
-    run() {}
+    async run() {
+        while (await this.step()) {}
+    }
 
     async step() {
+        console.log("step", this.stepCounter ++)
         // nodes that have not run this round yet and have either no input ports (Type.INPUT) or have all incoming data available (Type.PROCESSOR)
         let readyNodes = Object.values(this.nodesMap).filter(node => node.isReadyToRun())
         if (readyNodes.length === 0) {
             console.log("No nodes are ready to run, resetting graph")
             this.reset()
-            return
+            return false
         }
         for (let node of readyNodes) {
             let outgoingEdges = Object.values(this.edgesMap).filter(edge => edge.sourceNodeId === node.id)
             await node.run(outgoingEdges)
         }
         console.log("nodesMap", this.nodesMap)
+        return true
     }
 
     reset() {
@@ -31,5 +36,6 @@ export class Graph {
             node.ranThisRound = false
             node.incomingData = []
         }
+        this.stepCounter = 0
     }
 }
