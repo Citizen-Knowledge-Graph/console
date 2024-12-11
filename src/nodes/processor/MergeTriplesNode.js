d1f5e8ae8209e7a4d461d3360bd85ef807379772
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-12-11 15:54:57 +0100

    Init codemirror in non edit mode for processor nodes

--- src/nodes/processor/MergeTriplesNode.js
+++ src/nodes/processor/MergeTriplesNode.js
@@ -4,5 +4,6 @@ import { PORT } from "../nodeFactory.js"
 export class MergeTriplesNode extends CodeNode {
     constructor(name, x, y, editor, nodesMap) {
         super(name, [ PORT.TURTLE, PORT.TURTLE ], [ PORT.TURTLE ], x, y, editor, nodesMap)
+        super.initCodemirror("turtle", false)
     }
 }
--- src/nodes/processor/SparqlConstructExecNode.js
+++ src/nodes/processor/SparqlConstructExecNode.js
@@ -4,5 +4,6 @@ import { PORT } from "../nodeFactory.js"
 export class SparqlConstructExecNode extends CodeNode {
     constructor(name, x, y, editor, nodesMap) {
         super(name, [ PORT.TURTLE, PORT.SPARQL_CONSTRUCT ], [ PORT.TURTLE ], x, y, editor, nodesMap)
+        super.initCodemirror("turtle", false)
     }
 }
