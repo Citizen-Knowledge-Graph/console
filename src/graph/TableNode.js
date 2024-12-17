ea2e7eceb96012de3dc7df1e6d7f2e624e940858
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-12-18 00:11:32 +0100

    Render table

--- src/graph/TableNode.js
+++ src/graph/TableNode.js
@@ -9,7 +9,24 @@ export class TableNode extends Node {
         return `<table class="result-table"></table>`
     }
 
-    setValue(value) {
-        // TODO
+    setValue(tableData) {
+        let table = this.editor.container.querySelector(`#node-${this.id} .result-table`)
+        table.innerHTML = ""
+        let tr = document.createElement("tr")
+        table.appendChild(tr)
+        for (let header of tableData.headers) {
+            let th = document.createElement("th")
+            th.innerText = header
+            tr.appendChild(th)
+        }
+        for (let row of tableData.rows) {
+            let tr = document.createElement("tr")
+            table.appendChild(tr)
+            for (let cell of row) {
+                let td = document.createElement("td")
+                td.innerText = cell
+                tr.appendChild(td)
+            }
+        }
     }
 }
--- src/graph/processor/SparqlSelectExecNode.js
+++ src/graph/processor/SparqlSelectExecNode.js
@@ -5,15 +5,39 @@ import { runSparqlSelectQueryOnRdfString } from "../../utils.js"
 export class SparqlSelectExecNode extends TableNode {
     constructor(name, x, y, editor, nodesMap) {
         super(name, [ PORT.TURTLE, PORT.SPARQL ], [ PORT.CSV ], x, y, editor, nodesMap, TYPE.PROCESSOR)
+        this.prefixesMap = {
+            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            "xsd": "http://www.w3.org/2001/XMLSchema#",
+            "ff":  "https://foerderfunke.org/default#",
+            "sh":  "http://www.w3.org/ns/shacl#",
+        }
+    }
+
+    dePrefix(value) {
+        for (let [key, val] of Object.entries(this.prefixesMap)) {
+            if (value.startsWith(val)) {
+                return value.replace(val, key + ":")
+            }
+        }
+        return value
     }
 
     async runProcessor() {
         let turtle = this.incomingData.filter(port => port.dataType === PORT.TURTLE)[0].data
         let sparql = this.incomingData.filter(port => port.dataType === PORT.SPARQL)[0].data
-        let rows = await runSparqlSelectQueryOnRdfString(sparql, turtle)
-
-        // TODO
-
-        return ""
+        let results = await runSparqlSelectQueryOnRdfString(sparql, turtle)
+        let variables = Object.keys(results[0]) // assuming they don't change between rows
+        let rows = []
+        for (let result of results) {
+            let row = []
+            for (let variable of variables) {
+                row.push(this.dePrefix(result[variable]))
+            }
+            rows.push(row)
+        }
+        return {
+            headers: variables,
+            rows: rows
+        }
     }
 }
