3ffd581d81700d6122334e9b8ae274ecacc29538
Benjamin Degenhart <b.degenhart.work@gmail.com>
2024-12-19 00:33:45 +0100

    Text Viewer to pass on input unchanged

--- src/graph/view/TextViewNode.js
+++ src/graph/view/TextViewNode.js
@@ -3,7 +3,7 @@ import { PORT, TYPE } from "../nodeFactory.js"
 
 export class TextViewNode extends CodeNode {
     constructor(name, x, y, editor, nodesMap) {
-        super(name, [ PORT.ANY ], [], x, y, editor, nodesMap, TYPE.VIEW)
+        super(name, [ PORT.ANY ], [ PORT.ANY ], x, y, editor, nodesMap, TYPE.VIEW)
         super.initCodemirror("text/plain")
     }
 
