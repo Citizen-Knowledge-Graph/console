3d5529f7bb921e4aa0c41704f1ac404f022c9a25
benjaminaaron <benjaminaaron@users.noreply.github.com>
2024-12-19 19:52:58 +0000

    deploy: 61b469c7b6ea2b8e4fb70f276bc14317005a6026

---
+++ graph/nodeFactory.js
@@ -0,0 +1,100 @@
+import { PrefixesNode } from "./input/PrefixesNode.js"
+import { TurtleInputNode } from "./input/TurtleInputNode.js"
+import { SparqlInputNode } from "./input/SparqlInputNode.js"
+import { SparqlSelectExecNode } from "./processor/SparqlSelectExecNode.js"
+import { SparqlConstructExecNode } from "./processor/SparqlConstructExecNode.js"
+import { MergeTriplesNode } from "./processor/MergeTriplesNode.js"
+import { ShaclValidationNode } from "./processor/ShaclValidationNode.js"
+import { TextViewNode } from "./view/TextViewNode.js"
+import { SparqlInsertDeleteExecNode } from "./processor/SparqlInsertDeleteExecNode.js"
+
+export const TYPE = {
+    INPUT: 0,
+    PROCESSOR: 1,
+    VIEW: 2
+}
+
+export const PORT = {
+    ANY: 0,
+    TURTLE: 1,
+    SPARQL: 2,
+    CSV: 3
+}
+
+const nodeClasses = {
+    "PrefixesNode": PrefixesNode,
+    "TurtleInputNode": TurtleInputNode,
+    "SparqlInputNode": SparqlInputNode,
+    "SparqlSelectExecNode": SparqlSelectExecNode,
+    "SparqlConstructExecNode": SparqlConstructExecNode,
+    "MergeTriplesNode": MergeTriplesNode,
+    "ShaclValidationNode": ShaclValidationNode,
+    "TextViewNode": TextViewNode,
+    "SparqlInsertDeleteExecNode": SparqlInsertDeleteExecNode
+}
+
+const exampleValues = {
+    "ex_PrefixesNode_ff": `rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns#
+xsd: http://www.w3.org/2001/XMLSchema#
+ff:  https://foerderfunke.org/default#
+sh:  http://www.w3.org/ns/shacl#`,
+    "ex_TurtleInputNode_UP": `@prefix ff: <https://foerderfunke.org/default#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+
+ff:mainPerson a ff:Citizen ;
+    ff:hasBirthday "1990-01-02"^^xsd:date .`,
+    "ex_TurtleInputNode_RP1": `@prefix ff: <https://foerderfunke.org/default#> .
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+
+ff:SimpleBenefitShape1 a sh:NodeShape ;
+    sh:targetClass ff:Citizen ;
+    sh:property [
+        sh:path ff:hasAge ;
+        sh:minCount 1 ;
+        sh:minInclusive 18 ;
+    ] .`,
+    "ex_TurtleInputNode_RP2": `@prefix ff: <https://foerderfunke.org/default#> .
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+
+ff:SimpleBenefitShape2 a sh:NodeShape ;
+    sh:targetClass ff:Citizen ;
+    sh:property [
+        sh:path ff:hasResidence ;
+        sh:minCount 1 ;
+        sh:hasValue "Berlin" ;
+    ] .`,
+    "ex_SparqlInputNode_MatRule": `PREFIX ff: <https://foerderfunke.org/default#>
+CONSTRUCT {
+    ?person ff:hasAge ?age .
+} WHERE {
+    ?person ff:hasBirthday ?bday .
+    BIND(YEAR(NOW()) - YEAR(?bday) - IF(MONTH(NOW()) < MONTH(?bday) || (MONTH(NOW()) = MONTH(?bday) && DAY(NOW()) < DAY(?bday)), 1, 0) AS ?age) .
+}`,
+    "ex_SparqlInputNode_Select": `PREFIX ff: <https://foerderfunke.org/default#>
+SELECT * WHERE {
+    ?s ?p ?o.
+}`,
+    "ex_SparqlInputNode_InsertData": `PREFIX ff: <https://foerderfunke.org/default#>
+INSERT DATA {
+    ff:mainPerson ff:hasResidence "Berlin" .
+}`,
+    "ex_SparqlInputNode_DeleteInsert": `PREFIX ff: <https://foerderfunke.org/default#>
+DELETE {
+    ?user ff:hasBirthday ?bday .
+} INSERT {
+    ?user ff:hasResidence "Munich" .
+} WHERE { 
+    ?user a ff:Citizen ;
+        ff:hasBirthday ?bday .
+}`
+}
+
+export function createNode(command, name, x, y, editor, nodesMap, originalCommand) {
+    if (!nodeClasses[command]) {
+        console.warn(`No node class found for: ${command}`)
+        return null
+    }
+    let node = new nodeClasses[command](name, x, y, editor, nodesMap)
+    if (originalCommand) node.setValue(exampleValues[originalCommand])
+    return node
+}
---
+++ utils.js
@@ -0,0 +1,141 @@
+import { Parser, QueryEngine, rdf, Store, Validator, Writer } from "./assets/bundle.js"
+
+export async function fetchAsset(relPath) {
+    const response = await fetch("assets/" + relPath, {
+        method: "GET",
+        cache: "reload"
+    })
+    return await response.text()
+}
+
+// type: "text/turtle"
+export function download(content, type, filename) {
+    let blob = new Blob([content], {type: type})
+    let url = URL.createObjectURL(blob)
+    let a = document.createElement("a")
+    a.href = url
+    a.download = filename
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+    window.URL.revokeObjectURL(url)
+}
+
+/*
+let rpsDir = await fetchAsset("rps-dir.txt")
+
+<input type="file" id="fileUpload" />
+<div onclick="download('foo', 'text/plain', 'file.txt')">dev</div>-
+<div onclick="func()">dev</div>
+
+document.getElementById("fileUpload").addEventListener("change", function() {
+    const reader = new FileReader()
+    reader.onload = function(event) {
+        const content = event.target.result
+        console.log("File content:", content)
+    }
+    const file = this.files[0]
+    reader.readAsText(file)
+})
+*/
+
+export async function runSparqlSelectQueryOnRdfString(query, rdfStr) {
+    let store = new Store()
+    await addRdfStringToStore(rdfStr, store)
+    const queryEngine = new QueryEngine()
+    let bindingsStream = await queryEngine.queryBindings(query, { sources: [ store ] })
+    let bindings = await bindingsStream.toArray()
+    let results = []
+    bindings.forEach(binding => {
+        const variables = Array.from(binding.keys()).map(({ value }) => value)
+        let row = {}
+        variables.forEach(variable => {
+            row[variable] = binding.get(variable).value
+        })
+        results.push(row)
+    })
+    return results
+}
+
+export async function runSparqlConstructQueryOnRdfString(query, rdfStr) {
+    let store = new Store()
+    await addRdfStringToStore(rdfStr, store)
+    const queryEngine = new QueryEngine()
+    let quadsStream = await queryEngine.queryQuads(query, { sources: [ store ] })
+    return await quadsStream.toArray()
+}
+
+export async function runSparqlInsertDeleteQueryOnStore(query, store) {
+    const queryEngine = new QueryEngine()
+    await queryEngine.queryVoid(query, { sources: [store] })
+}
+
+export function addRdfStringToStore(rdfStr, store) {
+    return new Promise((resolve, reject) => {
+        const parser = new Parser()
+        parser.parse(rdfStr, (err, quad) => {
+            if (err) {
+                console.error(err)
+                reject(err)
+            }
+            if (quad) {
+                store.add(quad)
+            } else {
+                resolve(store)
+            }
+        })
+    })
+}
+
+export function serializeStoreToTurtle(store) {
+    return new Promise((resolve, reject) => {
+        let writer = new Writer({
+            prefixes: {
+                rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+                ff: "https://foerderfunke.org/default#",
+                sh: "http://www.w3.org/ns/shacl#",
+            }
+        })
+        store.getQuads().forEach(quad => writer.addQuad(quad))
+        writer.end((error, result) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+export function serializeDatasetToTurtle(dataset) {
+    return new Promise((resolve, reject) => {
+        let writer = new Writer({
+            prefixes: {
+                ff: "https://foerderfunke.org/default#",
+                sh: "http://www.w3.org/ns/shacl#",
+            }
+        })
+        dataset.forEach(quad => writer.addQuad(quad))
+        writer.end((error, result) => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+
+export async function runValidationOnStore(store) {
+    let dataset = rdf.dataset(store.getQuads())
+    let validator = new Validator(dataset, { factory: rdf, debug: false })
+    return await validator.validate({ dataset: dataset })
+}
+
+export function buildEdgeId(editorConnectionObj) {
+    let outId = editorConnectionObj.output_id
+    let outPortId = editorConnectionObj.output_class
+    let inId = editorConnectionObj.input_id
+    let inPortId = editorConnectionObj.input_class
+    return `${outId}-${outPortId}-${inId}-${inPortId}`
+}
