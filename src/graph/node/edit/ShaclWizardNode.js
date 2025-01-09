8a7edc2268933f203770163165d51fa41a71fda7
Benjamin Degenhart <b.degenhart.work@gmail.com>
2025-01-10 00:23:36 +0100

    Wip wizard node

--- src/graph/node/edit/ShaclWizardNode.js
+++ src/graph/node/edit/ShaclWizardNode.js
@@ -1,9 +1,12 @@
 import { Node } from "../Node.js"
 import { PORT, TYPE } from "../../nodeFactory.js"
+import { addRdfStringToStore, runSparqlSelectQueryOnStore } from "../../../utils.js"
+import { Store } from "../../../assets/bundle.js"
 
 export class ShaclWizardNode extends Node {
     constructor(initialValues, graph) {
         super(initialValues, graph, [ PORT.TURTLE ], [ PORT.TURTLE ], TYPE.EDIT)
+        this.store = null
     }
 
     getMainHtml() {
@@ -18,4 +21,33 @@ export class ShaclWizardNode extends Node {
         container.style.cursor = "default"
         container.addEventListener("mousedown", event => event.stopPropagation())
     }
+
+    enoughIncomingDataAvailable() {
+        return this.hasAsMuchIncomingDataAvailableAsIncomingEdges()
+    }
+
+    async processIncomingData() {
+        let container = this.nodeDiv.querySelector(".shacl-wizard-container")
+        while (container.firstChild) container.firstChild.remove()
+        this.store = new Store()
+        let input = this.incomingData[0].data
+        await addRdfStringToStore(input, this.store)
+
+        let query = `
+            PREFIX ff: <https://foerderfunke.org/default#>
+            PREFIX sh: <http://www.w3.org/ns/shacl#>
+            SELECT * WHERE {
+                ?nodeShape a sh:NodeShape ;
+                    sh:targetClass ?class .
+            }`
+        let rows = await runSparqlSelectQueryOnStore(query, this.store)
+        for (let row of rows) {
+            // TODO
+        }
+        return null
+    }
+
+    // can this be moved to Node and be a fallback there? check for unforeseen consequences
+    getValue() { return "" }
+    setValue(value) {}
 }
