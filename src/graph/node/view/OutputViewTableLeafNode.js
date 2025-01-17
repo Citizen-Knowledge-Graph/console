319ec8cef843dd18ee774a7b33d95eb2e8a4355b
Benjamin Degenhart <b.degenhart.work@gmail.com>
2025-01-17 14:45:22 +0100

    Live output viewer can't currently serve output ports

--- src/graph/node/view/OutputViewTableLeafNode.js
+++ src/graph/node/view/OutputViewTableLeafNode.js
@@ -3,7 +3,7 @@ import { TableNode } from "../TableNode.js"
 
 export class OutputViewTableLeafNode extends TableNode {
     constructor(initialValues, graph) {
-        super(initialValues, graph, [ PORT.CSV ], [ PORT.CSV ], TYPE.VIEW_LEAF)
+        super(initialValues, graph, [ PORT.CSV ], [], TYPE.VIEW_LEAF)
     }
 
     getMainHtml() {
