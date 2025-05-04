edbdd9dda93a5086c07130052aff50484850e6aa
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2025-05-04 10:18:01 +0200

    Add server generated by spring initializr
    https://start.spring.io

---
+++ server/src/main/java/org/foerderfunke/sem_ops_server/SemOpsServerApplication.java
@@ -0,0 +1,13 @@
+package org.foerderfunke.sem_ops_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemOpsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemOpsServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/org/foerderfunke/sem_ops_server/SemOpsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foerderfunke.sem_ops_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemOpsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
